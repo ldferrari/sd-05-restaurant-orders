ff65e98196d3dd310cc31f8321945de3e93aee5f
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2021-04-19 20:43:57 -0300

    req1 without exception treating

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -2,8 +2,8 @@ import csv
 from collections import defaultdict
 
 # 1. Em cada function, transformar orders_1.csv em objeto relevante
-
 # 2. Functions necessárias para achar os 4 elementos pedidos
+
 
 def get_all_dishes(path_to_file):
     all_dishes = set()
@@ -65,30 +65,38 @@ def most_ordered_dish_by_customer(path_to_file, customer):
     return dish
     # formato retornado: hamburger
 
+
 # 3.2 Contar vezes que cliente ('arnaldo') pediu prato ('hamburguer')
-def count_dishes_by_customer(path_to_file, customer, dish):
+def count_dish_customer(path_to_file, customer, dish):
     count_dish = most_ordered_dishes(path_to_file, customer)[dish]
     return count_dish
 
+
 # 3.3 Pratos que cliente ('joao') nunca pediu
 def get_never_ordered(path_to_file, customer):
-    return get_all_dishes(path_to_file).difference(dishes_by_customer(path_to_file, customer))
+    all = get_all_dishes(path_to_file)
+    by_customer = dishes_by_customer(path_to_file, customer)
+    return all.difference(by_customer)
+
 
 # 3.4 Dias que cliente ('joao') nunca foi na lanchonete
 def get_never_went(path_to_file, customer):
-    return get_all_days(path_to_file).difference(days_by_customer(path_to_file, customer))
+    all = get_all_days(path_to_file)
+    by_customer = days_by_customer(path_to_file, customer)
+    return all.difference(by_customer)
+
 
 # 4. Mandar as respostas no arquivo data/mkt_campaign.txt
 def analyze_log(path_to_file):
+    data_path = 'data/orders_1.csv'
     final_text = []
-    final_text.append(most_ordered_dish_by_customer('data/orders_1.csv', 'maria'))
-    final_text.append(count_dishes_by_customer('data/orders_1.csv', 'arnaldo', 'hamburguer'))
-    final_text.append(get_never_ordered('data/orders_1.csv', 'joao'))
-    final_text.append(get_never_went('data/orders_1.csv', 'joao'))
+    final_text.append(most_ordered_dish_by_customer(data_path, 'maria'))
+    final_text.append(count_dish_customer(data_path, 'arnaldo', 'hamburguer'))
+    final_text.append(get_never_ordered(data_path, 'joao'))
+    final_text.append(get_never_went(data_path, 'joao'))
     # print(final_text)
-    # ['hamburguer', 1, {'pizza', 'coxinha', 'misto-quente'}, {'segunda-feira', 'sabado'}]
-    file_path = "data/mkt_campaign.txt"
-    with open(file_path, "w") as final_file:
+    destination_path = "data/mkt_campaign.txt"
+    with open(destination_path, "w") as final_file:
         for element in final_text:
             final_file.write(str(element) + "\n")
             # https://stackoverflow.com/questions/37289951/how-to-write-to-a-csv-line-by-line
@@ -96,10 +104,6 @@ def analyze_log(path_to_file):
 
 
 if __name__ == "__main__":
-    # print(days_by_customer('data/orders_1.csv', 'joao'))
-    # print(dishes_by_customer('data/orders_1.csv', 'joao'))
-    # print(most_ordered_dish_by_customer('data/orders_1.csv', 'maria'))
-    # print(count_dishes_by_customer('data/orders_1.csv', 'arnaldo', 'hamburguer'))
     # print(get_never_ordered('data/orders_1.csv', 'joao'))
     # print(get_never_went('data/orders_1.csv', 'joao'))
     print(analyze_log('data/orders_1.csv'))
