4efca4d82ef54d6e3abe016e0e4616eef72b962a
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2021-04-19 16:44:20 -0300

    strategy req1

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,16 @@
+import csv
+
+# 1. Transformar csv (orders_1.csv) em set consultável
+
+# 2. Achar no set o prato mais pedido por 'maria'
+
+# 3. Achar no set quantas vezes 'arnaldo' pediu 'hamburguer'
+
+# 4. Achar no set quais pratos 'joao' nunca pediu
+
+# 5. Achar no set quais dias 'joao' nunca foi na lanchonete
+
+# 6. Mandar as respostas no arquivo data/mkt_campaign.txt
+
 def analyze_log(path_to_file):
     raise NotImplementedError
