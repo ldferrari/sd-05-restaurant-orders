762eeefc09c3fbf196ddb85a254ac329b1d05020
Douglas <douglaza@gmail.com>
2021-04-19 19:36:51 -0300

    req1.2 - file not found

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -3,3 +3,8 @@ import csv
 def analyze_log(path_to_file):
     if not path_to_file.endswith(".csv"):
         raise FileNotFoundError(f"No such file or directory: '{path_to_file}'")
+    try:
+        with open(path_to_file, encoding='utf-8') as csv_file:
+            return csv_file
+    except FileExistsError:
+        return 'File not found'
