fa8de3ab5ef81dc592c31caf3ec2b720773530cc
ederusena <eder.sena@live.com>
2021-04-20 19:12:29 -0300

    chore: requisito 1

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,7 @@
+import csv
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    with open(path_to_file, newline="") as csvfile:
+        data = csv.DictReader(csvfile, fieldnames=["nome", "prato", "dia"])
+        print(data)
