04926b0920e25acf9f3d2469dab47138db0c552d
ederusena <eder.sena@live.com>
2021-04-21 15:33:26 -0300

    chore: pratos que joao nunca pediu

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,4 +1,5 @@
 import csv
+from statistics import mode
 
 
 def analyze_log(path_to_file):
@@ -6,8 +7,6 @@ def analyze_log(path_to_file):
         data = csv.DictReader(csvfile, fieldnames=["nome", "prato", "dia"])
 
         """
-        Qual o prato mais pedido por 'maria'?
-        Quantas vezes 'arnaldo' pediu 'hamburguer'?
         Quais pratos 'joao' nunca pediu?
         Quais dias 'joao' nunca foi na lanchonete?
         """
@@ -15,17 +14,32 @@ def analyze_log(path_to_file):
         # Array para poder fazer a o mode para saber qual
         # foi o mais pedido
         pedidos_maria = [] 
-        # pedidos_arnaldo = 0
-        # pedidos_joao = set()
+        hamburguer_arnaldo = 0
         pratos = set()
         dias = set()
+        pratos_joao = set()
 
         for row in data:
             # {'hamburguer', 'coxinha', 'misto-quente', 'pizza'}
             pratos.add(row["prato"])
             # {'terça-feira', 'segunda-feira', 'sabado'}
             dias.add(row["dia"])
+            # Qual o prato mais pedido por 'maria'?
             if row["nome"] == "maria":
                 pedidos_maria.append(row["prato"])
-        print(pedidos_maria)
+            # Quantas vezes 'arnaldo' pediu 'hamburguer'?
+            if row["nome"] == "arnaldo" and row["prato"] == "hamburguer":
+                hamburguer_arnaldo += 1
+            if row["nome"] == "joao":
+                pratos_joao.add(row["prato"])
+            # Quais pratos 'joao' nunca pediu?
+
+        joao_nunca_pediu = pratos.difference(pratos_joao)
+        
+        campaign_text = open("./data/mkt_campaign.txt", "w")
+        campaign_text.write(
+            f"{mode(pedidos_maria)}\n"
+            f"{hamburguer_arnaldo}\n"
+            f"{joao_nunca_pediu}\n"
+        )
 analyze_log('data/orders_1.csv')
