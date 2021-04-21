720e0b27b1b4ba08daccbd694dd95cb34d470096
lucascv <lucascv333@gmail.com>
2021-04-21 11:17:04 -0300

    criando __init__, __len__,add_new_order and get_most_per_costumer

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,14 +1,26 @@
+import operator
+
+
 class TrackOrders:
     def __init__(self):
         self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append({"costumer": costumer, "order": order, "day": day})
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        pedidos = dict({})
+        for order in self.orders:
+            if order["costumer"] == costumer:
+                if order["order"] in pedidos:
+                    pedidos[order["order"]] += 1
+                else:
+                    pedidos[order["order"]] = 1
+        mais_pedido = max(pedidos.items(), key=operator.itemgetter(1))[0]
+        return mais_pedido
 
     def get_order_frequency_per_costumer(self, costumer, order):
         pass
