545d69687e4d1a2d2a2f6fd34ce02b9f979abcfd
ederusena <eder.sena@live.com>
2021-04-22 13:49:36 -0300

    chore: add new order, adicionar um pedido

--- src/track_orders.py
+++ src/track_orders.py
@@ -2,26 +2,41 @@ class TrackOrders:
     def __init__(self):
         self.orders = []
 
+    # 2.1 - Será validado se, ao instanciar a classe TrackOrders
+    # pela primeira vez, o método retorna a quantidade de pedidos
+    # é igual a zero.
     def __len__(self):
         return len(self.orders)
 
+    # 2.2 - Será validado se, ao executar o método add_new_order, 
+    # o método deve adicionar um pedido.
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
+    # 2.3 - Será validado se, ao executar get_most_ordered_dish_per_costumer, 
+    # o método retorna o prato mais pedido.
     def get_most_ordered_dish_per_costumer(self, costumer):
         pass
 
+    # 2.4 - Será validado se, ao executar 
+    # get_never_ordered_per_costumer, o método retorna o 
+    # pedido que o cliente nunca fez.
     def get_order_frequency_per_costumer(self, costumer, order):
         pass
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
 
+    # 2.5 - Será validado se, ao executar get_days_never_visited_per_costumer,
+    # o método retorna o dias que o cliente nunca visitou.
     def get_days_never_visited_per_costumer(self, costumer):
         pass
 
+    # 2.6 - Será validado se, ao executar o método get_busiest_day,
+    # o método retorna o dia mais movimentado.
     def get_busiest_day(self):
         pass
-
+    # 2.7 - Será validado se, ao executar o método get_least_busy_day,
+    # o método retorna o dia menos movimentado.
     def get_least_busy_day(self):
         pass
