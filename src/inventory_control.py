a9adac614ac20c68241042a85506a6ed47d7e581
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-04-24 20:43:41 -0300

    Muito booommm, mini chef

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -16,9 +16,34 @@ class InventoryControl:
             'massa': 50,
             'frango': 50,
         }
+        
+        self.to_buy = {
+            'pao': 0,
+            'carne': 0,
+            'queijo': 0,
+            'molho': 0,
+            'presunto': 0,
+            'massa': 0,
+            'frango': 0,
+        }
 
     def add_new_order(self, costumer, order, day):
-        pass
+        ingredients = self.ingredients.get(order)
+        for ingredient in ingredients:
+            qtd = self.minimum_inventory.get(ingredient)
+            self.to_buy[ingredient] += 1
+            if qtd == 0:
+                return False
+            self.minimum_inventory[ingredient] = qtd - 1
 
     def get_quantities_to_buy(self):
-        pass
+        return self.to_buy
+    
+    def get_available_dishes(self):
+        dishes =  set(self.ingredients.keys())
+        for dish in self.ingredients.keys():
+            for ingredient in self.ingredients.get(dish):
+                if self.minimum_inventory.get(ingredient) <= self.to_buy.get(ingredient):
+                    dishes.remove(dish)
+                    break
+        return dishes
